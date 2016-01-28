9f55530b429695186d8c180542b5206c1c31f9de
DeerMichel <mh74182@gmail.com>
2016-01-28 20:54:13 +0100

    Added screen

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ $(function() {
     $(this).delay(index * 200).fadeIn(1000);
   }); --> CSS ANIMATION!*/
 
+  var currentProject = "";
+
+
   function sayHello() {
 
     dropbox.getAccountInfo(function(error, info) {
@@ -26,26 +29,29 @@ $(function() {
 
   }
 
-  function addProjectTile(title, preview) {
+  function addProjectTile(title, index) {
 
     /*dropbox.makeUrl(title + "/.preview.png", {download: true}, function(error, result) {*/
+
+    // create html
+    var newTile =
+      // "<div class='tile'><div class='tileoverlay'>Title</div><img src='img/demo.jpg'></div>"
+
+      $("<div>").attr("class", "tile").append(
+        $("<div>").attr("class", "tileoverlay").html(title)
+      ).append(
+        $("<img>")
+      )
+      .hide().velocity("fadeIn", {duration: 1000, delay: index*200})
+
+    .appendTo("#tilescontainer");
+
+    // add preview images
     dropbox.readFile(title + "/.preview.png", {blob: true}, function(error, data) {
       if (error) {
         alert(error);
       } else {
-
-        $("#tilescontainer").append(
-          // "<div class='tile'><div class='tileoverlay'>Title</div><img src='img/demo.jpg'></div>"
-
-          $("<div>").attr("class", "tile").append(
-            $("<div>").attr("class", "tileoverlay").html(title)
-          ).append(
-            $("<img>").attr("src", URL.createObjectURL(data))
-          )
-          .hide().velocity("fadeIn", {duration: 1000})
-
-        );
-
+        $(newTile).find("img").attr("src", URL.createObjectURL(data)).velocity({opacity: 1}, {duration: 1000});
       }
     });
 
@@ -62,16 +68,20 @@ $(function() {
       } else {
         $("body").attr("class", "projects");
         for (var i = 0; i < entries.length; i++) {
-          addProjectTile(entries[i]);
+          addProjectTile(entries[i], i);
         }
         sayHello();
       }
     });
   });
 
+  // open project
   $("#tilescontainer").on("click", ".tile", function() {
+    currentProject = $(this).find(".tileoverlay").html();
     $("#tilescontainer").velocity("fadeOut", {duration: 500, complete: function() {
       $("body").attr("class", "editor");
+      $("#title").velocity("stop");
+      $("#title").html(currentProject);
       $("#edittext").velocity("fadeIn", {duration: 500});
     }});
   });
@@ -92,14 +102,42 @@ $(function() {
 
   });
 
+  // go back
   $("#controlbar .ion-chevron-left").click(function() {
+
+    // from editor to project overview
+    if ($("body").hasClass("editor")) {
+      $("#edittext").velocity("fadeOut", {duration: 500, complete: function() {
+        $("body").attr("class", "projects");
+        $("#title").html("Arendelle Studio");
+        $("#tilescontainer").velocity("fadeIn", {duration: 500});
+      }});
+    }
+
+    // from screen to editor
+    else if ($("body").hasClass("screen")) {
+      $("#screen").velocity("fadeOut", {duration: 500, complete: function() {
+        $("body").attr("class", "editor");
+        $("#title").html(currentProject);
+        $("#edittext").velocity("fadeIn", {duration: 500});
+      }});
+    }
+
+  });
+
+  // run project
+  $("#controlbar .ion-play").click(function() {
+
     $("#edittext").velocity("fadeOut", {duration: 500, complete: function() {
-      $("body").attr("class", "projects");
-      $("#tilescontainer").velocity("fadeIn", {duration: 500});
+      $("body").attr("class", "screen");
+      $("#screen").velocity("fadeIn", {duration: 500});
+      // TODO: insert engine here
     }});
+
   });
 
 });
+
 
 
 // APIs
